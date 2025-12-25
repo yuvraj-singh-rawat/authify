38777283fbba4854b3a2fed20c3a89476e795da8
Yuvraj Singh Rawat <127573114+yuvraj-singh-rawat@users.noreply.github.com>
2025-12-25 18:53:23 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.use(cors({
-    origin: ["http://localhost:5173","https://authify-pink.vercel.app"],
+    origin: process.env.CLIENT_URL,
     credentials: true
 }));
 
